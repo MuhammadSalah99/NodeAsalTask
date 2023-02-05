3d69efe497db6502cec03a32f416b7211574d54a
Mohammad Junidy <mohmmedjunide73@gmail.com>
2023-02-05 20:28:41 +0200

    added search with price and unts range

--- server/routes/Books.js
+++ server/routes/Books.js
@@ -11,14 +11,24 @@ router.get("/", async (req, res) => {
 
 router.get("/search/:phrase", async (req, res) => {
   const phrase = req.params.phrase;
+  const priceStart = parseInt(req.query.priceStart);
+  const priceEnd = parseInt(req.query.priceEnd);
+  const unitStart = parseInt(req.query.unitStart);
+  const unitEnd = parseInt(req.query.unitEnd);
   const listOfBooks = await Book.findAll({
     where: {
-      [Op.or]: [
-        { BookId: { [Op.like]: "%" + phrase + "%" } },
-        { BookTitle: { [Op.like]: "%" + phrase + "%" } },
-        { BookPublisher: { [Op.like]: "%" + phrase + "%" } },
-        { BookAuthor: { [Op.like]: "%" + phrase + "%" } },
-        { PublishDate: { [Op.like]: "%" + phrase + "%" } },
+      [Op.and]: [
+        { Price: { [Op.between]: [priceStart, priceEnd] } },
+        { Units: { [Op.between]: [unitStart, unitEnd] } },
+        {
+          [Op.or]: [
+            { BookId: { [Op.like]: "%" + phrase + "%" } },
+            { BookTitle: { [Op.like]: "%" + phrase + "%" } },
+            { BookPublisher: { [Op.like]: "%" + phrase + "%" } },
+            { BookAuthor: { [Op.like]: "%" + phrase + "%" } },
+            { PublishDate: { [Op.like]: "%" + phrase + "%" } },
+          ],
+        },
       ],
     },
   });
@@ -26,31 +36,69 @@ router.get("/search/:phrase", async (req, res) => {
 });
 
 router.get("/searchId/:BookId", async (req, res) => {
+  const priceStart = parseInt(req.query.priceStart);
+  const priceEnd = parseInt(req.query.priceEnd);
+  const unitStart = parseInt(req.query.unitStart);
+  const unitEnd = parseInt(req.query.unitEnd);
   const BookId = req.params.BookId;
   const listOfBooks = await Book.findAll({
     where: {
-      BookId: { [Op.like]: `%${BookId}%` },
+      [Op.and]: [
+        { BookId: { [Op.like]: `%${BookId}%` } },
+        { Price: { [Op.between]: [priceStart, priceEnd] } },
+        { Units: { [Op.between]: [unitStart, unitEnd] } },
+      ],
     },
   });
   res.json(listOfBooks);
 });
 
 router.get("/searchPublisher/:BookPublisher", async (req, res) => {
+  const priceStart = parseInt(req.query.priceStart);
+  const priceEnd = parseInt(req.query.priceEnd);
+  const unitStart = parseInt(req.query.unitStart);
+  const unitEnd = parseInt(req.query.unitEnd);
   const BookPublisher = req.params.BookPublisher;
   const listOfBooks = await Book.findAll({
     where: {
-      BookPublisher: { [Op.like]: `%${BookPublisher}%` },
+      [Op.and]: [
+        { BookPublisher: { [Op.like]: `%${BookPublisher}%` } },
+        { Price: { [Op.between]: [priceStart, priceEnd] } },
+        { Units: { [Op.between]: [unitStart, unitEnd] } },
+      ],
     },
   });
   res.json(listOfBooks);
 });
 
 router.get("/searchTitle/:BookTitle", async (req, res) => {
+  const priceStart = parseInt(req.query.priceStart);
+  const priceEnd = parseInt(req.query.priceEnd);
+  const unitStart = parseInt(req.query.unitStart);
+  const unitEnd = parseInt(req.query.unitEnd);
   const BookTitle = req.params.BookTitle;
   const listOfBooks = await Book.findAll({
-    where: {
-      BookTitle: { [Op.like]: `%${BookTitle}%` },
-    },
+    [Op.and]: [
+      { BookTitle: { [Op.like]: `%${BookTitle}%` } },
+      { Price: { [Op.between]: [priceStart, priceEnd] } },
+      { Units: { [Op.between]: [unitStart, unitEnd] } },
+    ],
+  });
+  res.json(listOfBooks);
+});
+
+router.get("/tags/:Tags", async (req, res) => {
+  const priceStart = parseInt(req.query.priceStart);
+  const priceEnd = parseInt(req.query.priceEnd);
+  const unitStart = parseInt(req.query.unitStart);
+  const unitEnd = parseInt(req.query.unitEnd);
+  const Tags = req.params.Tags;
+  const listOfBooks = await Book.findAll({
+    [Op.and]: [
+      { Tags: { [Op.like]: "%" + Tags + "%" } },
+      { Price: { [Op.between]: [priceStart, priceEnd] } },
+      { Units: { [Op.between]: [unitStart, unitEnd] } },
+    ],
   });
   res.json(listOfBooks);
 });
